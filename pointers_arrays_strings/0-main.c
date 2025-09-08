6823a0204031bc5787b165907fd3745f5d5a817f
Ali Abdullah <aliabdullah1215@icloud.com>
2025-09-08 13:36:27 +0000

    Implement _memset function

---
+++ pointers_arrays_strings/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char buffer[10];
+    int i;
+
+    _memset(buffer, 'A', 10);
+
+    for (i = 0; i < 10; i++)
+        printf("%c", buffer[i]);
+    printf("\n");
+    return (0);
+}
