2a6d0a5446177853c3ff3682f0339e0331d238df
Ali Abdullah <aliabdullah1215@icloud.com>
2025-09-12 06:30:34 +0000

    Add _strlen_recursion function (task 2)

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,7 +8,8 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')      /* شرط التوقف عند نهاية النص */
-        return (0);
-    return (1 + _strlen_recursion(s + 1)); /* استدعاء الدالة على باقي النص */
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
 }
