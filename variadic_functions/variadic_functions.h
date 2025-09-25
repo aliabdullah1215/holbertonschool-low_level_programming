ee6da363ff7994f9ee424fdaf53e274a8c640fd6
Ali Abdullah <aliabdullah1215@icloud.com>
2025-09-25 12:21:43 +0000

    Add task 3: print_all variadic function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -13,4 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 /* Task 2 */
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/* Task 3 */
+void print_all(const char * const format, ...);
+
 #endif /* VARIADIC_FUNCTIONS_H */
