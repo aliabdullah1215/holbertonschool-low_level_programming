b575b3305c1a242fe1822ee9c3399522a204b21b
Ali Abdullah <aliabdullah1215@icloud.com>
2025-10-18 14:26:30 +0000

    Fix: handle read return value and partial writes; correct usage/close messages (task 3)

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,18 +2,6 @@
 #include <stdio.h>
 
 /**
- * error_exit - prints an error message and exits with a given code
- * @code: exit status
- * @msg: message to print
- * @arg: filename or fd
- */
-void error_exit(int code, const char *msg, const char *arg)
-{
-	dprintf(STDERR_FILENO, msg, arg);
-	exit(code);
-}
-
-/**
  * main - copies the content of a file to another file
  * @ac: argument count
  * @av: argument vector
@@ -23,40 +11,74 @@ void error_exit(int code, const char *msg, const char *arg)
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
-	ssize_t rbytes, wbytes;
+	ssize_t rbytes;
 	char buf[1024];
 
 	if (ac != 3)
-		error_exit(97, "Usage: cp %s %s\n", "file_from file_to");
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(99);
 	}
 
+	/* اقرأ على دفعات 1024 بايت، وتأكد من كتابة كل البايتات المقروءة */
 	while ((rbytes = read(fd_from, buf, 1024)) > 0)
 	{
-		wbytes = write(fd_to, buf, rbytes);
-		if (wbytes != rbytes)
+		ssize_t written = 0;
+
+		while (written < rbytes)
 		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to %s\n", av[2]);
+			ssize_t w = write(fd_to, buf + written, rbytes - written);
+
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+				if (close(fd_from) == -1)
+					dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+				if (close(fd_to) == -1)
+					dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+				exit(99);
+			}
+			written += w;
 		}
 	}
+	/* فشل في القراءة */
 	if (rbytes == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		if (close(fd_to) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(98);
+	}
 
+	/* إغلاق الملفات مع التحقق من الأخطاء */
 	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", av[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", av[2]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 
 	return (0);
 }
