bed8bda580d8bac143a5677870fb1c7691b31d10
Ali Abdullah <aliabdullah1215@icloud.com>
2025-10-21 08:01:54 +0000

    feat: implement hash_table_get (task 4)

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -2,11 +2,11 @@
 
 /**
  * hash_table_get - retrieves a value associated with a key
- * @ht: the hash table you want to look into
+ * @ht: the hash table
  * @key: the key you are looking for
  *
- * Return: the value associated with the element,
- * or NULL if key couldn’t be found
+ * Return: the value associated with the key,
+ *         or NULL if the key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
